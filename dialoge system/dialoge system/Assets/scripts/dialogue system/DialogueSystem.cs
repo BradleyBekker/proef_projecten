55559282207203220da82f88c7721527bc96adcc
BradleyBekker <thekid72gaming@gmail.com>
2019-09-13 10:08:35 +0200

    added a basic dialogue system

---
+++ dialoge system/dialoge system/Assets/scripts/dialogue system/DialogueSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [SerializeField] private Text _textbox;
+    private UnitDialogue UD;
+    private int _index = 0;
+    bool isDialogueRunning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UD = GetComponent<UnitDialogue>();
+       _textbox  = GameObject.Find("DialogueBox").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDialogueRunning && Input.anyKeyDown)
+        {
+            StartDialogue();
+        }
+        else if (isDialogueRunning && Input.anyKeyDown)
+        {
+            ProceedDialogue();
+        }
+    }
+
+    public void StartDialogue()
+    {
+        isDialogueRunning = true;
+        _index = 0;
+       
+
+        _textbox.text = UD.dialogue[_index];
+    }
+    public void ProceedDialogue()
+    {
+        _index++;
+
+        if (UD.dialogue.Count -1 < _index)
+        {
+            print("stopped");
+            StopDialogue();
+            return;
+        }
+       
+        _textbox.text = UD.dialogue[_index];
+
+    }
+
+    public void StopDialogue()
+    {
+        _textbox.text = "";
+        isDialogueRunning = false;
+    } 
+
+}
---
+++ dialoge system/dialoge system/Assets/scripts/dialogue system/UnitDialogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UnitDialogue : MonoBehaviour
+{
+    [SerializeField]
+    private string path;
+    private string json;
+    public List<string> dialogue = new List<string>();
+
+    private void Start()
+    {
+        
+    }
+}
